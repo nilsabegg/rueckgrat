4a7038aaa13ef9217ffeeda52f1f83e742a87446
Nils Abegg <nils.abegg@ffuf.de>
2012-09-14 00:03:42 +0200

    Admin Controller Fixes

--- src/Rueckgrat/Admin/Controller.php
+++ src/Rueckgrat/Admin/Controller.php
@@ -7,49 +7,49 @@ use Rueckgrat\View\View as View;
 class Controller extends \Rueckgrat\Controller\Controller
 {
 
-        /**
-         * isSecured
-         *
-         * Indicates if the controller perfoms actions
-         * for authenticated users only.
-         *
-         * @var boolean
-         */
-        protected $isSecured = true;
+    /**
+     * isSecured
+     *
+     * Indicates if the controller perfoms actions
+     * for authenticated users only.
+     *
+     * @var boolean
+     */
+    protected $isSecured = true;
 
-        public function __construct($action, $pimple)
-        {
-            parent::__construct($action, $pimple);
-            $this->createNavigation();
-        }
+    public function __construct($action, $pimple)
+    {
+        parent::__construct($action, $pimple);
+        $this->createNavigation();
+    }
 
-        /**
-         * createNavigation
-         *
-         * Creates the navigation bar for the admin section
-         *
-         * @return void
-         */
-        protected function createNavigation()
-        {
-            $this->pimple['view.rootPath'] = 'admin/_navigation';
-            $navigationView = $this->pimple['view'];
-            $navigationView->set('rootUrl', $this->getRootUrl());
-            $this->template->set('navigation', $navigationView->render());
-        }
+    /**
+     * createNavigation
+     *
+     * Creates the navigation bar for the admin section
+     *
+     * @return void
+     */
+    protected function createNavigation()
+    {
+        $this->pimple['view.rootPath'] = 'admin/_navigation';
+        $navigationView = $this->pimple['view'];
+        $navigationView->set('rootUrl', $this->getRootUrl());
+        $this->template->set('navigation', $navigationView->render());
+    }
 
-        /**
-         * createTemplate
-         *
-         * Creates the page template.
-         *
-         * @return void
-         */
-        protected function createTemplate()
-        {
+    /**
+     * createTemplate
+     *
+     * Creates the page template.
+     *
+     * @return void
+     */
+    protected function createTemplate()
+    {
 
-            $this->template = new View('admin', $this->config);
+        $this->template = new View('admin', $this->config);
 
-        }
+    }
 
 }
